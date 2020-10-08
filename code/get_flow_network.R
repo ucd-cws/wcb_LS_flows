697b9930f0bb3f5ced7f6758983a348ac2be5de9
Ryan Peek <rapeek@ucdavis.edu>
2020-10-07 19:21:10 -0700

    initialize and add flow code

---
+++ code/get_flow_network.R
@@ -0,0 +1,101 @@
+# Get nhd flow network data
+
+# Wed Oct  7 14:53:33 2020 ------------------------------
+
+# this pulls flow network from NHD and NHD+ attributes for cleaning
+# see here for NHDplus National Data
+# https://www.epa.gov/waterdata/nhdplus-national-data
+
+# Libraries ---------------------------------------------------------------
+
+library(sf)
+library(nhdplusTools)
+library(tidyverse)
+library(mapview)
+mapviewOptions(fgb=FALSE)
+
+# Identify the COMID ------------------------------------------------------
+
+# comid for confluence segment Shasta-Little Shasta
+peace <- st_sfc(st_point(c(-122.51016, 41.70364)), crs=4326) # peacemaker gage
+uspt <- st_sfc(st_point(c(-122.34792, 41.73355)), crs=4326) # upstream gage
+
+(peace_comid <- discover_nhdplus_id(point = peace))
+(uspt_comid <- discover_nhdplus_id(point = uspt))
+
+pts <- tibble("site"=c("peace","uspt"), "comid"=c(peace_comid, uspt_comid))
+
+# what sources of data?
+# discover_nldi_sources()$source
+
+# Use NHD to Download Flow Network ----------------------------------------
+
+# make a list of comids
+pt_list <- map(pts$comid, ~list(featureSource = "comid", featureID=.x))
+
+# Get stream segments, this can take a minute
+us_main <- map(pt_list[1], ~navigate_nldi(nldi_feature = .x,
+                                       mode="UM",
+                                       distance_km = 110))
+
+us_trib <- map(pt_list[1], ~navigate_nldi(nldi_feature = .x,
+                                       mode="UT",
+                                       distance_km = 110))
+
+# make a single flat layer
+um_flat <- us_main %>%
+  set_names(., pts$comid[1]) #%>%
+  #map2(pts$site, ~mutate(.x, site=.y))
+
+
+ut_flat <- us_trib %>%
+  set_names(., pts$comid[1])
+
+
+# bind together
+# mainstems
+lshasta_um <- do.call(what = sf:::rbind.sf,
+                      args = um_flat) %>%
+  mutate(type="UM")
+
+# Tributaries
+lshasta_ut <- do.call(what = sf:::rbind.sf,
+                      args = ut_flat) %>%
+  mutate(type="UT")
+
+# preview
+mapview(lshasta_um, zcol="nhdplus_comid", legend = F) + mapview(lshasta_ut, color="slateblue", lwd=0.5, legend=F)
+
+
+# download the added data
+output_file_download <- file.path(here::here("data", "nhdplus_little_shasta.gpkg"))
+
+# this retrieves all the detailed information
+output_file_download <-subset_nhdplus(comids = lshasta_um$nhdplus_comid,
+                                      output_file = output_file_download,
+                                      flowline_only = FALSE,
+                                      overwrite = TRUE,
+                                      nhdplus_data = "download", return_data = FALSE)
+
+
+# READ IN DATA AND CHECK --------------------------------------------------
+
+# see what layers
+sf::st_layers("data/nhdplus_little_shasta.gpkg")
+
+# this explains attributes:
+# https://s3.amazonaws.com/edap-nhdplus/NHDPlusV21/Data/NationalData/0Release_Notes_NationalData_Seamless_GeoDatabase.pdf
+
+# read data in: flowlines
+flowlines <- sf::read_sf("data/nhdplus_little_shasta.gpkg", "NHDFlowline_Network")
+catchment <- sf::read_sf("data/nhdplus_little_shasta.gpkg", "CatchmentSP")
+
+# quick plot
+plot(catchment$geom,  lwd = 4, border = "gray30")
+plot(sf::st_geometry(flowlines), lwd = 7, col = alpha("blue", 0.5), add=TRUE)
+plot(lshasta_ut$geometry, lwd = 1.2, col = "dodgerblue", add = TRUE)
+
+# mapview
+mapview(catchment, color="gray50", col.regions="gray50", alpha.regions=0, alpha=0.5, lwd=2) +
+  mapview(flowlines, zcol="ftype") +
+  mapview(lshasta_ut, color="dodgerblue", lwd=0.75, alpha=0.9)
