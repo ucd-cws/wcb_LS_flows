8f1229d6db90dcca5dac9af399ce6f2dd1998e42
Ryan Peek <rapeek@ucdavis.edu>
2020-10-09 12:52:24 -0700

    update visulization and save mapview map raw

--- code/map_flow_network.R
+++ code/map_flow_network.R
@@ -18,15 +18,16 @@ st_layers("data/nhdplus_little_shasta.gpkg")
 flowlines <- sf::read_sf("data/nhdplus_little_shasta.gpkg", "NHDFlowline_Network")
 catchment <- sf::read_sf("data/nhdplus_little_shasta.gpkg", "CatchmentSP")
 lshasta_ut <- sf::read_sf("data/nhdplus_little_shasta.gpkg", "flowlines_ut_lshasta")
-
+lshasta_um <- sf::read_sf("data/nhdplus_little_shasta.gpkg", "flowlines_um_lshasta")
+lshasta_basin <- sf::read_sf("data/nhdplus_little_shasta.gpkg", "basin_lshasta")
 
 # Static Plot -------------------------------------------------------------
 
 # quick static plot
-plot(catchment$geom,  lwd = 4, border = "gray30")
-plot(st_geometry(flowlines), lwd = 7, col = alpha("blue", 0.5), add=TRUE)
+plot(catchment$geom,  lwd = 2, border = alpha("gray80", 0.5))
+plot(st_geometry(lshasta_basin), lwd = 2.5, col=alpha("forestgreen",0.3), border="darkgreen", add=TRUE)
+plot(st_geometry(lshasta_um), lwd = 7, col = alpha("maroon", 0.5), add=TRUE)
 plot(st_geometry(lshasta_ut), lwd = 1.2, col = "dodgerblue", add = TRUE)
-
 
 
 # nhdplusTools plot and summary stats for comid SEG ------------------------
@@ -39,9 +40,7 @@ nldi_feature <- list(featureSource = "comid",
                      featureID = "3917946")
 
 # pull flowlines and full basin, make plot of catchments and flowlines
-data <- plot_nhdplus(nldi_feature, flowline_only = FALSE)
-
-# hmmm
+# data <- plot_nhdplus(nldi_feature, flowline_only = FALSE)
 
 # Use the COMID to look up NLDI characteristics and return a summary table
 # see here: https://waterdata.usgs.gov/blog/nldi_update/
@@ -73,17 +72,18 @@ DT::datatable(outlet_total)
 # Mapview -----------------------------------------------------------------
 
 
+flowlines_trim <- flowlines %>% select(id:flowdir, ftype:pathlength)
+
 # mapview
-map_raw <- mapview(catchment, color="gray50", col.regions="gray50", alpha.regions=0, alpha=0.5, lwd=2) +
-  mapview(flowlines, zcol="ftype") +
-  mapview(lshasta_ut, color="dodgerblue", lwd=0.75, alpha=0.9)
+(map_raw <- mapview(catchment, color="gray50", col.regions="gray50", alpha.regions=0, alpha=0.5, lwd=2, layer="Catchment") +
+    mapview(lshasta_um, color="darkblue", lwd=6, alpha=0.5, layer="Mainstem") +
+  mapview(flowlines_trim, zcol="ftype", layer="Flowlines"))
 
-
-# so mainstem seems to be ok, but the associated tributaries are a jumble of diversions, canals, etc.
+# save as flowline_map_raw.html, then zip and push zipped to github
 
 # this is a way to edit that map should we choose to
-library(mapedit)
-library(leafem)
-library(leaflet)
+# library(mapedit)
+# library(leafem)
+# library(leaflet)
 
-#lshasta_network <- map_raw %>% mapedit::editMap(targetLayerId="lshasta_ut", editor="leafpm")
+# lshasta_network <- map_raw %>% mapedit::editMap(targetLayerId="lshasta_ut", editor="leafpm")
